4fb75eec34ccac7f748c5ccaee4095f74572eaac
FedericoJigen <guitarman94bis@gmail.com>
2020-07-30 17:53:11 +0200

    Service OK, Notification not (check Run() in MyService)

--- app/src/main/java/com/bttoy/service_task_template/MainActivity.java
+++ app/src/main/java/com/bttoy/service_task_template/MainActivity.java
@@ -1,15 +1,26 @@
 package com.bttoy.service_task_template;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Build;
 import android.os.Bundle;
 import android.widget.Switch;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     protected static final String TAG = "MainActivity";
     private static final String CHANNEL_ID = "001";
     private NotificationManager mNotification = null;
+    private BroadcastReceiver receiver = null;
     private Switch taskSw, serviceSw;
     private boolean compliant;
 
@@ -21,5 +32,79 @@ public class MainActivity extends AppCompatActivity {
         taskSw = findViewById(R.id.taskSwitch);
         serviceSw = findViewById(R.id.serviceSwitch);
 
+        /*
+            Setting up listeners for switches
+         */
+        taskSw.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if(isChecked){
+                //TODO: activate task
+            } else {
+                //TODO: terminate task
+            }
+        });
+
+        serviceSw.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if(isChecked){
+                Intent intent = new Intent(getApplicationContext(), MyService.class);
+                startService(intent);
+            } else {
+                Intent intent = new Intent(getApplicationContext(), MyService.class);
+                stopService(intent);
+            }
+        });
+
+        /*
+            Setting up BReceiver into a LocalBroadcastManager
+         */
+        createNotificationChannel();
+        receiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                String[] message = intent.getStringExtra("update").split(":");
+                showNotification(message[0], message[1].trim());
+            }
+        };
+        LocalBroadcastManager.getInstance(getApplicationContext()).registerReceiver(receiver, new IntentFilter("SERVICE_UPDATE"));
     }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        LocalBroadcastManager.getInstance(getApplicationContext()).registerReceiver(receiver, new IntentFilter());
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        LocalBroadcastManager.getInstance(getApplicationContext()).unregisterReceiver(receiver);
+    }
+
+    private void createNotificationChannel(){
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = getString(R.string.channel_name);
+            String description = getString(R.string.channel_description);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager mNotification = getSystemService(NotificationManager.class);
+            if (mNotification!=null) mNotification.createNotificationChannel(channel);
+            else Toast.makeText(this, "Notification manager is null",Toast.LENGTH_SHORT);
+        }
+    }
+    private void showNotification(final String title, final String msg) {
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        PendingIntent pi = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        Notification notification = new Notification.Builder(this, CHANNEL_ID)
+                .setContentTitle(title)
+                .setContentText(msg)
+                .setAutoCancel(true)
+                .setContentIntent(pi)
+                .build();
+        if(mNotification!= null) mNotification.notify(0, notification);
+    }
+
 }
--- app/src/main/java/com/bttoy/service_task_template/MyService.java
+++ app/src/main/java/com/bttoy/service_task_template/MyService.java
@@ -1,4 +1,83 @@
 package com.bttoy.service_task_template;
 
-public class MyService {
+import android.app.Service;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.SystemClock;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
+
+public class MyService extends Service {
+    protected static final String TAG = "MyService";
+    private Handler handler = null;
+    private Thread worker = null;
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        /*
+            Wanting a starting server: binding is of no use.
+         */
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId){
+        handler = new Handler();
+        worker = new Thread(new Runnable(){
+            @Override
+            public void run() {
+                /*
+                    TODO: Il service va. Il problema è che non manda le notifiche. Perché? Investiga!
+                 */
+                Log.d(TAG, "Running service job!");
+                tellMain("Service: Hello from Service!!");
+                handler.postDelayed(this, 20000l);
+            }
+        });
+        if (startServiceJob()){
+            Log.d(TAG, "Job started!");
+            tellMain("Debug: Job started!");
+        }
+        else {
+            Log.d(TAG, "Some problem happened with worker!");
+            tellMain("Debug: some problem happened with worker!");
+        }
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy(){
+        stopServiceJob();
+        super.onDestroy();
+    }
+
+    private boolean startServiceJob(){
+        worker.start();
+        if (!worker.isAlive()){
+            return false;
+        }
+        return true;
+    }
+
+    private void stopServiceJob(){
+        if (worker.isAlive()){
+            try {
+                worker.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        handler.removeCallbacksAndMessages(null);
+    }
+
+    private void tellMain(String msg){
+        Intent intent = new Intent("SERVICE_UPDATE");
+        // You can also include some extra data.
+        intent.putExtra("update", msg);
+        LocalBroadcastManager.getInstance(getApplicationContext()).sendBroadcast(intent);
+    }
 }
