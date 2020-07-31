d1ace7d5903d15f92c88f472c99b4b26b4d82c88
FedericoJigen <guitarman94bis@gmail.com>
2020-07-31 10:24:38 +0200

    Service Ok, Notification OK, Broadcast Management OK - Notification moved to Service - Notification NEEDS "setSmallIcon" to work

--- app/src/main/java/com/bttoy/service_task_template/MainActivity.java
+++ app/src/main/java/com/bttoy/service_task_template/MainActivity.java
@@ -1,25 +1,18 @@
 package com.bttoy.service_task_template;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.localbroadcastmanager.content.LocalBroadcastManager;
-
-import android.app.Notification;
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Switch;
-import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
 public class MainActivity extends AppCompatActivity {
     protected static final String TAG = "MainActivity";
-    private static final String CHANNEL_ID = "001";
-    private NotificationManager mNotification = null;
     private BroadcastReceiver receiver = null;
     private Switch taskSw, serviceSw;
     private boolean compliant;
@@ -56,12 +49,11 @@ public class MainActivity extends AppCompatActivity {
         /*
             Setting up BReceiver into a LocalBroadcastManager
          */
-        createNotificationChannel();
         receiver = new BroadcastReceiver() {
             @Override
             public void onReceive(Context context, Intent intent) {
+                Log.d(TAG, "Received intent!");
                 String[] message = intent.getStringExtra("update").split(":");
-                showNotification(message[0], message[1].trim());
             }
         };
         LocalBroadcastManager.getInstance(getApplicationContext()).registerReceiver(receiver, new IntentFilter("SERVICE_UPDATE"));
@@ -70,7 +62,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onResume(){
         super.onResume();
-        LocalBroadcastManager.getInstance(getApplicationContext()).registerReceiver(receiver, new IntentFilter());
+        LocalBroadcastManager.getInstance(getApplicationContext()).registerReceiver(receiver, new IntentFilter("SERVICE_UPDATE"));
     }
 
     @Override
@@ -79,32 +71,5 @@ public class MainActivity extends AppCompatActivity {
         LocalBroadcastManager.getInstance(getApplicationContext()).unregisterReceiver(receiver);
     }
 
-    private void createNotificationChannel(){
-        // Create the NotificationChannel, but only on API 26+ because
-        // the NotificationChannel class is new and not in the support library
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            CharSequence name = getString(R.string.channel_name);
-            String description = getString(R.string.channel_description);
-            int importance = NotificationManager.IMPORTANCE_DEFAULT;
-            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
-            channel.setDescription(description);
-            // Register the channel with the system; you can't change the importance
-            // or other notification behaviors after this
-            NotificationManager mNotification = getSystemService(NotificationManager.class);
-            if (mNotification!=null) mNotification.createNotificationChannel(channel);
-            else Toast.makeText(this, "Notification manager is null",Toast.LENGTH_SHORT);
-        }
-    }
-    private void showNotification(final String title, final String msg) {
-        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-        PendingIntent pi = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-        Notification notification = new Notification.Builder(this, CHANNEL_ID)
-                .setContentTitle(title)
-                .setContentText(msg)
-                .setAutoCancel(true)
-                .setContentIntent(pi)
-                .build();
-        if(mNotification!= null) mNotification.notify(0, notification);
-    }
 
 }
--- app/src/main/java/com/bttoy/service_task_template/MyService.java
+++ app/src/main/java/com/bttoy/service_task_template/MyService.java
@@ -1,10 +1,13 @@
 package com.bttoy.service_task_template;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Handler;
 import android.os.IBinder;
-import android.os.SystemClock;
 import android.util.Log;
 
 import androidx.annotation.Nullable;
@@ -14,6 +17,8 @@ public class MyService extends Service {
     protected static final String TAG = "MyService";
     private Handler handler = null;
     private Thread worker = null;
+    private static final String CHANNEL_ID = "001";
+    private NotificationManager mNotification = null;
 
     @Nullable
     @Override
@@ -26,13 +31,11 @@ public class MyService extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId){
+        mNotification = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
         handler = new Handler();
         worker = new Thread(new Runnable(){
             @Override
             public void run() {
-                /*
-                    TODO: Il service va. Il problema è che non manda le notifiche. Perché? Investiga!
-                 */
                 Log.d(TAG, "Running service job!");
                 tellMain("Service: Hello from Service!!");
                 handler.postDelayed(this, 20000l);
@@ -79,5 +82,22 @@ public class MyService extends Service {
         // You can also include some extra data.
         intent.putExtra("update", msg);
         LocalBroadcastManager.getInstance(getApplicationContext()).sendBroadcast(intent);
+        showNotification("Service", msg);
+    }
+
+    private void showNotification(final String title, final String msg) {
+        /*
+            Finalmente la notification worka. E' obbligatoria una icon... (setSmallIcon)
+         */
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        PendingIntent pi = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        Notification notification = new Notification.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.notify_example_icon)
+                .setContentTitle(title)
+                .setContentText(msg)
+                .setAutoCancel(true)
+                .setContentIntent(pi)
+                .build();
+        if (mNotification != null) mNotification.notify(0, notification);
     }
 }
